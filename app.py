ad3599d57b30fc3782dfa9e2dc41d769c804753a
Shofieu <muhsoft@gmail.com>
2019-12-11 16:31:56 +0700

    ganti keyword untuk send dm

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def start():
 
                 if len(message) is not 0 and len(message) < 280:
                     if "prikitiw" in message:
-                        message = message.replace("prikitiw", "")
+                        message = message.replace("/tgmd", "")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")
